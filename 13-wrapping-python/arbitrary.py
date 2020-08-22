dfd60b6154f0aa1f727fa4e228037c836f90a0f3
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-08-22 08:41:00 -0400

    resolve linter

--- 13-wrapping-python/arbitrary.py
+++ 13-wrapping-python/arbitrary.py
@@ -3,14 +3,37 @@ someFunction
 """
 
 
-def someFunction(text):
+def some_function(text: str) -> int:
+    """
+    string in a string
+    :param text:
+    :return:
+    """
     print(f'You passed this Python program {text} from C! Congratulations!')
     return 12345
 
 
-def multiply(a, b):
-    print("Will compute", a, "times", b)
-    c = 0
-    for i in range(0, a):
-        c = c + b
-    return c
+def multiply(multiplier: float, multiplicand: float) -> float:
+    """
+    multiplication
+    :param multiplier:
+    :param multiplicand:
+    :return:
+    """
+    print("Will compute", multiplier, "times", multiplicand)
+    result = 0
+    for _ in range(0, multiplier):
+        result = result + multiplicand
+    return result
+
+
+def main():
+    """
+    entrypoint
+    :return:
+    """
+    print(f"2 * 3 = {multiply(2, 3)}")
+
+
+if __name__ == "__main__":
+    main()
