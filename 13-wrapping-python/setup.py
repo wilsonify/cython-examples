d3cce013b0364b1b6e4b242770826171a1ab6ede
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-08-22 11:48:45 -0400

    create setup.py for arbitrary_package

---
+++ 13-wrapping-python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arbitrary_package',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
