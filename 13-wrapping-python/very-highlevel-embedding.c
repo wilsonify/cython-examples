a4a90ad0a15cdc640db2d791aabbc078ecd67d36
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-08-22 08:33:29 -0400

    add highlevel example of calling python string and file from c

---
+++ 13-wrapping-python/very-highlevel-embedding.c
@@ -0,0 +1,25 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+int
+main(int argc, char *argv[])
+{
+    wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+    if (program == NULL) {
+        fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+        exit(1);
+    }
+    Py_SetProgramName(program);  /* optional but recommended */
+    Py_Initialize();
+    PyRun_SimpleString("from time import time,ctime\n"
+                       "print('Today is', ctime(time()))\n");
+
+    FILE *file = _Py_fopen("arbitrary.py", "r+");
+    PyRun_SimpleFile(file, "arbitrary.py");
+
+    if (Py_FinalizeEx() < 0) {
+        exit(120);
+    }
+    PyMem_RawFree(program);
+    return 0;
+}
