9176c5bb39dbdb1499335412703c2a997711bf41
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-08-22 09:20:20 -0400

    remove duplicate files define a main

--- 01-essentials/01-comparing-python-c-and-cython/fib.py
+++ 01-essentials/01-comparing-python-c-and-cython/fib.py
@@ -1,11 +1,31 @@
+"""
+example of a compute limited function
+"""
+import sys
+
+
 def fib(n):
+    """
+    calculate the nth fibonacci number
+    :param n:
+    :return:
+    """
     a, b = 0, 1
     for i in range(n):
         a, b = a + b, a
     return a
 
-if __name__ == '__main__':
-    import sys
-    arg, numiter = list(map(int, sys.argv[1:]))
-    for i in range(numiter):
+
+def main(arg, numiter):
+    """
+    call fib many times
+    :param arg:
+    :param numiter:
+    """
+    for _ in range(numiter):
         fib(arg)
+
+
+if __name__ == '__main__':
+    arg_outer, numiter_outer = list(map(int, sys.argv[1:]))
+    main(arg_outer, numiter_outer)
