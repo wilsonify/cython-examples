cf7214ba8c7932a88ccdeb773cda2efb15e4a62a
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-08-22 10:57:58 -0400

    resolve linter

--- 11-spectral-norm/01-pure-python/spectral_norm.py
+++ 11-spectral-norm/01-pure-python/spectral_norm.py
@@ -12,8 +12,10 @@ import sys
 from array import array
 from math import sqrt
 
+
 def A(i, j):
     return 1.0 / (((i + j) * (i + j + 1) >> 1) + i + 1)
+
 
 def A_times_u(u, v):
     u_len = len(u)
@@ -29,12 +31,12 @@ def A_times_u(u, v):
 def At_times_u(u, v):
     u_len = len(u)
 
-    for i in range(u_len):
+    for u_i in range(u_len):
         partial_sum = 0
-        for j in range(u_len):
-            partial_sum += A(j, i) * u[j]
+        for u_j in range(u_len):
+            partial_sum += A(u_j, u_i) * u[u_j]
 
-        v[i] = partial_sum
+        v[u_i] = partial_sum
 
 
 def B_times_u(u, out, tmp):
@@ -55,12 +57,12 @@ def spectral_norm(n):
 
     for ue, ve in zip(u, v):
         vBv += ue * ve
-        vv  += ve * ve
+        vv += ve * ve
 
     return sqrt(vBv / vv)
 
 
 if __name__ == "__main__":
-    n = int(sys.argv[1])
-    spec_norm = spectral_norm(n) 
+    n_outer = int(sys.argv[1])
+    spec_norm = spectral_norm(n_outer)
     print(("%0.9f" % spec_norm))
